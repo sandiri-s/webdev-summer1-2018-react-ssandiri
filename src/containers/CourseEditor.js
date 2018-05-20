45a24aae2e3a8a1b35c542b9a4a4ee559de5efc0
Shravan Kumar Reddy Sandiri <shravanreddy.sandiri@gmail.com>
2018-05-20 01:40:42 -0400

    added CourseEditor container component

---
+++ src/containers/CourseEditor.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ModuleList from './ModuleList'
+import LessonTabs from './LessonTabs'
+
+export default class CourseEditor
+  extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {courseId: ''};
+    this.selectCourse = this.selectCourse.bind(this);
+  }
+
+  componentDidMount() {
+    this.selectCourse
+    (this.props.match.params.courseId);
+  }
+
+  selectCourse(courseId) {
+    this.setState({courseId: courseId});
+  }
+
+  render() { return(
+    <div>
+      <h2>Editing course: {this.state.courseId}</h2>
+      <div className="row">
+        <div className="col-4">
+          <ModuleList courseId={this.state.courseId}/>
+        </div>
+        <div className="col-8">
+          <LessonTabs/>
+        </div>
+      </div>
+    </div>
+  );}}
