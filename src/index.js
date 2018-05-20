6f15cc1f7fdd2e25cf18457cdf7a09e24000d4cd
Shravan Kumar Reddy Sandiri <shravanreddy.sandiri@gmail.com>
2018-05-19 23:33:41 -0400

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './hello'
+import CourseManager from './containers/CourseManager'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import Hello from './components/Hello'
+import Stateless from './components/Stateless'
+import ModuleListItem from "./components/ModuleListItem";
+import ModuleList2 from "./containers/ModuleList2"
+import App from "./examples/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+  <div className="container-fluid">
+    <CourseManager/>
+  </div>,
+  document.getElementById('root')
+);
